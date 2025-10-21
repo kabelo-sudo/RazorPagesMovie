46ed0c849bcc791cd336ed91e4ba4bd58cfaf9ba
kabelo-sudo <kabelogivenesssosibo@gmail.com>
2025-10-21 08:48:38 +0200

    updated OnGetAsync code

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -34,7 +34,14 @@ namespace RazorPagesMovie.Pages.Movies
 
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            var movies = from m in _context.Movie
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            Movie = await movies.ToListAsync();
         }
     }
 }
