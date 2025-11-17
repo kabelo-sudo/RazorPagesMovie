732d153e039a629d951df8a93d612fdff8210615
katliey7029 <prosperitynong@gmail.com>
2025-11-17 14:34:42 +0200

    Added a code for uploading movies

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -28,4 +28,7 @@ public class Movie
     [StringLength(5)]
     [Required]
     public string Rating { get; set; } = string.Empty;
+
+    public string? MovieFilePath { get; set; }
+
 }
--- Pages/Movies/Create.cshtml.cs
+++ Pages/Movies/Create.cshtml.cs
@@ -7,14 +7,16 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
+using Microsoft.AspNetCore.Http;
+using System.IO;
 
 namespace RazorPagesMovie.Pages.Movies
 {
     public class CreateModel : PageModel
     {
-        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+        private readonly RazorPagesMovieContext _context;
 
-        public CreateModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
+        public CreateModel(RazorPagesMovieContext context)
         {
             _context = context;
         }
@@ -27,12 +29,41 @@ namespace RazorPagesMovie.Pages.Movies
         [BindProperty]
         public Movie Movie { get; set; } = default!;
 
-        // For more information, see https://aka.ms/RazorPagesCRUD.
+        // This stores the uploaded movie file
+        [BindProperty]
+        public IFormFile MovieFile { get; set; }
+
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
             {
                 return Page();
+            }
+
+            // Handle movie upload
+            if (MovieFile != null)
+            {
+                // Folder to save movies
+                var movieFolder = Path.Combine("wwwroot", "movies");
+
+                // Create folder if it doesn't exist
+                if (!Directory.Exists(movieFolder))
+                {
+                    Directory.CreateDirectory(movieFolder);
+                }
+
+                // Generate a unique file name
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(MovieFile.FileName);
+                var filePath = Path.Combine(movieFolder, fileName);
+
+                // Save the movie file
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await MovieFile.CopyToAsync(stream);
+                }
+
+                // Save the file path to the database
+                Movie.MovieFilePath = "/movies/" + fileName;
             }
 
             _context.Movie.Add(Movie);
